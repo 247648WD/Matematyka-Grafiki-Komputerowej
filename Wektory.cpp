82405d7761b4731a0c9a9061e85d5428e0f79c38
wfrasunku <afrasun@wp.pl>
2024-10-14 23:48:53 +0200

    Code segreagted into cpp and headers

---
+++ Wektory.cpp
@@ -0,0 +1,113 @@
+#include "Wektory.h"
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+Wektory::Wektory() {}
+
+Wektory::Wektory(double x, double y, double z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Wektory::~Wektory() {}
+
+double Wektory::get_x() {
+    return this->x;
+}
+
+double Wektory::get_y() {
+    return this->y;
+}
+
+double Wektory::get_z() {
+    return this->z;
+}
+
+void Wektory::set_x(double x) {
+    this->x = x;
+}
+
+void Wektory::set_y(double y) {
+    this->y = y;
+}
+
+void Wektory::set_z(double z) {
+    this->z = z;
+}
+
+void Wektory::print() {
+    cout << this->x << ", " << this->y << ", " << this->z << endl;
+}
+
+Wektory Wektory::operator +(const Wektory& v1) {
+    return Wektory(this->x + v1.x, this->y + v1.y, this->z + v1.z);
+}
+
+Wektory Wektory::operator -(const Wektory& v1) {
+    return Wektory(this->x - v1.x, this->y - v1.y, this->z - v1.z);
+}
+
+Wektory Wektory::operator *(const double s) {
+    return Wektory(this->x * s, this->y * s, this->z * s);
+}
+
+Wektory Wektory::operator /(const double s) {
+    if (s != 0) {
+        return Wektory(this->x / s, this->y / s, this->z / s);
+    }
+    else {
+        cout << "Dzielnik nie moze byc rowny 0!" << endl;
+        return Wektory(this->x, this->y, this->z);
+    }
+}
+
+Wektory Wektory::operator +=(const Wektory& v1) {
+    return Wektory(this->x + v1.x, this->y + v1.y, this->z + v1.z);
+}
+
+Wektory Wektory::operator -=(const Wektory& v1) {
+    return Wektory(this->x - v1.x, this->y - v1.y, this->z - v1.z);
+}
+
+Wektory Wektory::operator *=(const double s) {
+    return Wektory(this->x * s, this->y * s, this->z * s);
+}
+
+Wektory Wektory::operator /=(const double s) {
+    if (s != 0) {
+        return Wektory(this->x / s, this->y / s, this->z / s);
+    }
+    else {
+        cout << "Dzielnik nie moze byc rowny 0!" << endl;
+        return Wektory(this->x, this->y, this->z);
+    }
+}
+
+double Wektory::length() {
+    return sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
+}
+
+Wektory Wektory::normalizacja() {
+    double d = this->length();
+    return Wektory(this->x / d, this->y / d, this->z / d);
+}
+
+double Wektory::iloczyn_skalarny(Wektory b) {
+    return this->x * b.x + this->y * b.y + this->z * b.z;
+}
+
+Wektory Wektory::iloczyn_wektorowy(Wektory a) {
+    return Wektory(this->y * a.z - this->z * a.y,
+        this->z * a.x - this->x * a.z,
+        this->x * a.y - this->y * a.x);
+}
+
+double Wektory::katy(Wektory v) {
+    double sum = this->iloczyn_skalarny(v);
+    double kat = acos(sum / (this->length() * v.length()));
+    return kat;
+}
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "Wektory.h"
+
+using namespace std;
+
+void checkWektory(Wektory v1, Wektory v2);
+
+int main() {
+    Wektory v1 = Wektory(0, 3, 0);
+    Wektory v2 = Wektory(5, 5, 0);
+    Wektory a_b = Wektory();
+    Wektory b_a = Wektory();
+
+    Wektory v3 = Wektory(4, 5, 1);
+    Wektory v4 = Wektory(4, 1, 3);
+    Wektory v5 = Wektory();
+
+    // zad.2
+    cout << "---------- zad.2 ----------" << endl;
+    a_b = v1 + v2;
+    a_b.print();
+
+    b_a = v2 + v1;
+    b_a.print();
+
+    checkWektory(a_b, b_a);
+
+    // zad.3
+    cout << endl << "---------- zad.3 ----------" << endl;
+    cout << v1.katy(v2) << endl;
+
+    // zad.4
+    cout << endl << "---------- zad.4 ----------" << endl;
+    v5 = v4.iloczyn_wektorowy(v3);
+    v5.print();
+
+    // zad.5
+    cout << endl << "---------- zad.5 ----------" << endl;
+    v5 = v5.normalizacja();
+    v5.print();
+
+    return 0;
+}
+
+void checkWektory(Wektory v1, Wektory v2) {
+    if (v1.get_x() == v2.get_x() && v1.get_y() == v2.get_y() && v1.get_z() == v2.get_z())
+        cout << "Wektory sa takie same" << endl;
+    else
+        cout << "Wektory sa rozne" << endl;
+}
